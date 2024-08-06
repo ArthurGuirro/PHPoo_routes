c0851b6b3e61d244b89efc205bdf3c469f622a6c
ArthurGuirro <arthurguirro@gmail.com>
2024-08-06 18:05:29 -0300

    Finalizando a parte de paginação, aula 23

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,44 +4,33 @@ namespace app\controllers;
 use app\controllers\Controller;
 use app\database\Filters;
 use app\database\models\User;
+use app\database\Pagination;
 
 class HomeController extends Controller
 {
     public function index()
     {   
 
-        $arr = ['name'=>'Robson','color'=>'red'];
-        $uri=http_build_query(array_merge($arr, $_GET));
+        // $arr = ['name'=>'Robson','color'=>'red'];
+        // $uri=http_build_query(array_merge($arr, $_GET));
+        // dd($uri);
 
-        dd($_GET);
-
-        // $filters = new Filters;
+        $filters = new Filters;
+        $filters->where('users.id', '>', 1);
         // $filters->join('posts', 'users.id', '=', 'posts.userId', 'left join');
-        // $filters->where('users.id', '<', 100);
-        // $user = new User;
-        // $user->setFilters($filters);
-        // $user->setFields('users.id,firstName,lastName,title');
-        // $userFound = $user->fetchAll();
-        // dd($userFound);
-        // $user = new User;
-        // $updated =  $user->update('id',4,['firstName'=>'Tutu', 'lastName'=>'Mascote']);
-        // dd($updated);
-        // $created = $user->create([
-        //     'firstName'=>"gpt",
-        //     'lastName'=>"da silva",
-        //     'email'=>"gp@t.com",
-        //     'passwords'=>"1234"
-        // ]);
-        // dd($created);
-        // foreach ($userFound as $user1) {
-        //     dd($user1->teste());
-        // }
-        //dd($usersFound);
-        // $filters->dump();
-        // $user = new User;
-        // $user->fetch();
 
-        $this->view('home', ['title' => 'Home']);//error 12:50
+        $pagination = new Pagination;
+        $pagination->setItemsPerPage(1);
+        
+        $user = new User;
+        $user->setFields('users.id,firstName,lastName');
+        $user->setFilters($filters);
+        $user->setPagination($pagination);
+        $usersFound = $user->fetchAll();
+
+        // dd($usersFound);
+
+        $this->view('home', ['title' => 'Home', 'users' => $usersFound, 'pagination' => $pagination]);//error 12:50
     }
 }
 
--- app/database/Pagination.php
+++ app/database/Pagination.php
@@ -26,6 +26,16 @@ class Pagination
         $this->itemsPerPage = $itemsPerPage;
     }
 
+    public function getTotal()
+    {
+        return $this->totalItems;
+    }
+
+    public function getPerPage()
+    {
+        return $this->itemsPerPage;
+    }
+
     private function calculations()
     {
         $this->currentPage = $_GET['page'] ?? 1;
@@ -44,15 +54,41 @@ class Pagination
 
     public function links()
     {
-        // $links = "<ul class='pagination'>";
+        $links = "<ul class='pagination'>";
 
-        // if ($this->currentPage > 1)
-        // {
-        //     $previous = $this->currentPage - 1;
-        //     $linkPage = http_build_query();
-        // }
+        if ($this->currentPage > 1)
+        {
+            $previous = $this->currentPage - 1;
+            $linkPage = http_build_query(array_merge($_GET, [$this->pageIdentifier => $previous]));
+            $first = http_build_query(array_merge($_GET, [$this->pageIdentifier => 1]));
+            $links.= "<li class='page-item'> <a href='?{$linkPage}' class='page-link'>Anterior</a> </li>";
+            $links.= "<li class='page-item'> <a href='?{$first}' class='page-link'>Primeira</a> </li>";
+            
+        }
+
+        for ($i=$this->currentPage - $this->linksPerPage; $i <= $this->currentPage + $this->linksPerPage; $i++) 
+        { 
+            if ($i > 0 && $i <= $this->totalPages)
+            {
+                $class = $this->currentPage === $i ? 'active' : '';
+                $linkPage = http_build_query(array_merge($_GET, [$this->pageIdentifier => $i]));
+                $links.= "<li class='page-item {$class}'> <a href='?{$linkPage}' class='page-link'> {$i} </a> </li>";
+            }
+        }
+
+        if ($this->currentPage < $this->totalPages)
+        {
+            $next = $this->currentPage + 1;
+            $linkPage = http_build_query(array_merge($_GET, [$this->pageIdentifier => $next]));
+            $last = http_build_query(array_merge($_GET, [$this->pageIdentifier => $this->totalPages]));
+            $links.= "<li class='page-item'> <a href='?{$linkPage}' class='page-link'>Próxima</a> </li>";
+            $links.= "<li class='page-item'> <a href='?{$last}' class='page-link'>Última</a> </li>";
+            
+        }
         
-        // $links.="</ul>";
+        $links.="</ul>";
+
+        return $links;
     }
 }
 
